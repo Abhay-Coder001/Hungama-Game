7880adda5e682712e4e6874c46e641668ccfa058
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2022-04-19 18:36:23 +0530

    All done now!

--- main.py
+++ main.py
@@ -4,13 +4,14 @@
 import random
 import time
 # Initial steps to invite in the game.
-print("\n Welcome to Hangman game \n")
-name = input("Enter your name :")
-print("Hello"+name+"! Best of luck!")
+print("Welcome to Hangman game \n")
+name = input("Enter your name : ")
+print("Hello "+name+"! Best of luck!")
 time.sleep(2) #time.sleep(2) basically 2 indicates here the delay of next line will appear in 2 seconds.
-print("The game is about to start!\n Let's play Hangman!")
+print("The game is about to start!\nLet's play Hangman!")
 time.sleep(3) #same goes for here but time delay will be 3 seconds.
-
+print("Words for guess :-\nJanuary,","border,","image,","film,","promise,","kids,","lungs,","doll,","rhyme,","damage,","plants.")
+time.sleep(2)
 #Now we create a main function
 def main(): 
     #All these used in below code afterwards..
@@ -32,7 +33,7 @@ def main():
 def play_loop(): #This function takes argument to either continue the game after it is played once or end it according to what user suggest.
     global play_game
     while play_game not in ['Y','y','n','N']:
-        play_game = input("Do you want to play again?\n Y = yes   N = no")
+        play_game = input("Do you want to play again?\n Y = yes   N = no""\n")
     if play_game == "y":
         main()
     elif play_game == "n":
@@ -46,7 +47,7 @@ def hangman():
     global already_guessed
     global play_game
     limit = 5
-    guess = input("This is the Hangman Word: " + display + " Enter your guess: \n")
+    guess = input("This is the Hangman Word: " + display + " \nEnter your guess: ")
     guess = guess.strip() #Removes the letter from the given word.
     if len(guess.strip()) == 0 or len(guess.strip()) >= 2 or guess <= "9":
         print("Invalid Input, Try a letter\n")
